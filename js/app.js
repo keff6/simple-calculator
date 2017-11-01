ba806b5b337f5b035376bab644afe170f1242ad4
keff6 <kev.fallas@gmail.com>
2017-10-31 19:28:05 -0600

    Adding keyboard input and style fixes

--- js/app.js
+++ js/app.js
@@ -1,49 +1,64 @@
 (function (doc) {
-  
+
   //Constants
   const UI_displayCurrent = doc.getElementById('display-result'),
-        UI_displaySubtotal = doc.getElementById('display-subtotal'),
-        UI_numbersGrid = doc.querySelector('.numbers-grid'),
-        UI_operandsGrid = doc.querySelector('.operands-grid'),
-        UI_btnReset = doc.getElementById("btn-reset");  
-  
+    UI_displaySubtotal = doc.getElementById('display-subtotal'),
+    UI_numbersGrid = doc.querySelector('.numbers-grid'),
+    UI_operandsGrid = doc.querySelector('.operands-grid'),
+    UI_btnReset = doc.getElementById("btn-reset");
+
   // Numbers
-  function appendNumber(e){  
-    if(typeof e.target.value !== 'undefined'){
+  function appendNumber(e) {
+    if (typeof e.target.value !== 'undefined') {
       UI_displaySubtotal.value += e.target.value;
-    }      
-  }  
-  
-  // Operands
-  function appendOperand(e){
-    let operand = e.target.value;    
+    }
+  }
+
+  //Manages keboard inputs
+  function keyboardManager(e) {
+    const regDigits = /^\d+$/;
+    const regOperands = /[\/\+\-\*\.]/; 
+    let input = String.fromCharCode(e.keyCode);
     
-    if(operand === "="){
+    if(e.keyCode === 13){
       getResult();
-    }else{ 
-      if(Number(UI_displayCurrent.value) !== 0){
+    }else if(regDigits.test(String.fromCharCode(e.keyCode))||
+             regOperands.test(String.fromCharCode(e.keyCode))){
+      UI_displaySubtotal.value += String.fromCharCode(e.keyCode);
+    }    
+  }
+
+  // Operands
+  function appendOperand(e) {
+    let operand = e.target.value;
+
+    if (operand === "=") {
+      getResult();
+    } else {
+      if (Number(UI_displayCurrent.value) !== 0) {
         UI_displaySubtotal.value = UI_displayCurrent.value;
       }
-      UI_displaySubtotal.value += operand;        
-    }   
-  }  
-  
+      UI_displaySubtotal.value += operand;
+    }
+  }
+
   // Functionality  
-  function getResult(){
-    try{
+  function getResult() {
+    try {
       UI_displayCurrent.value = math.eval(UI_displaySubtotal.value);
-    }catch(error){      
+    } catch (error) {
       console.log("ERR " + error);
-    }    
-  }  
-    
-  function cleanDisplayValues(){    
+    }
+  }
+
+  function cleanDisplayValues() {
     UI_displaySubtotal.value = "";
     UI_displayCurrent.value = "";
-  }  
-  
+  }
+
   //Event Listeners
-  UI_numbersGrid.addEventListener('click', appendNumber, false);
+  document.addEventListener('keypress', keyboardManager, false);
+  UI_numbersGrid.addEventListener('click', appendNumber, false);  
   UI_operandsGrid.addEventListener('click', appendOperand, false);
   UI_btnReset.addEventListener('click', cleanDisplayValues, false);
 
